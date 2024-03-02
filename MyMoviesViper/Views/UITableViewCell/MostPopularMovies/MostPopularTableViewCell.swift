f703a395c8340b583eac03b2ab8b253c5376dac4
LUIS <luis.enrique.enc@gmail.com>
2024-03-01 22:10:11 -0600

    [ADD] se agrega cocoa touch con xib

---
+++ MyMoviesViper/Views/UITableViewCell/MostPopularMovies/MostPopularTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MostPopularTableViewCell.swift
+//  MyMoviesViper
+//
+//  Created by LUIS GONZALEZ on 01/03/24.
+//
+
+import UIKit
+
+class MostPopularTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
