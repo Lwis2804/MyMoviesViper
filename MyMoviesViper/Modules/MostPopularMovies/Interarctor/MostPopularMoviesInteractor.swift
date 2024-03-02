84bfe6fa83cc73cf3a1e73de675d17fa34365eed
LUIS <luis.enrique.enc@gmail.com>
2024-03-01 22:27:48 -0600

    [ADD] se hace cambio en el table View

--- MyMoviesViper/Modules/MostPopularMovies/Interarctor/MostPopularMoviesInteractor.swift
+++ MyMoviesViper/Modules/MostPopularMovies/Interarctor/MostPopularMoviesInteractor.swift
@@ -4,8 +4,23 @@
 //  
 //  ViperTemplate v.0.0.1 - (2023, NS-Bionick Development Team)
 
-import Foundation
+import UIKit
 
+class MostPupularViewController : UIViewController { //como vincular con el xib 
+   
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+// MARK: - P R E S E N T E R · T O · V I E W
 class MostPopularMoviesInteractor: MostPopularMovies_PresenterToInteractorProtocol {
     weak var presenter: MostPopularMovies_InteractorToPresenterProtocol?
 
